3d1662aa6a255bb0d1df78e7ddde49eddb9035a9
Chris <chris@chriswetherill.me>
2014-05-16 09:53:21 -0400

    Added blog theme, assets

---
+++ blog/wp-content/themes/twentyfourteen/footer.php
@@ -0,0 +1,20 @@
+			<div class="footer">
+                <div class="copyright">
+                    <a rel="license" href="/license">
+                        <img src="/assets/CC0.png" style="border-style: none;" width="88px" height="31px" alt="CC0" />
+                    </a>
+				</div>
+				<div class="gplus">
+					<form role="search" method="get" class="search-form" action="http://chriswetherill.me/blog/">
+				<label>
+					<span class="screen-reader-text">Search for:</span>
+					<input type="search" class="search-field" placeholder="Search..." value="" name="s" title="Search for:">
+				</label>
+				<input type="submit" class="search-submit" value="Search">
+			</form>
+					<br />
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
