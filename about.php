58df95b7b1bc8a222015a965565dd29da0a6169f
faulconbridge <chris@tbmh.org>
2017-04-25 14:27:58 -0400

    source PHP headers from all files

--- about.php
+++ about.php
@@ -1,46 +1,7 @@
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-us">
-<head>
-  <meta charset="utf-8">
-  <meta name="author" content="Chris Wetherill">
-  <meta name="description" content="Trivia, nonsense, and other goings on by Chris Wetherill.">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
-  <script src="js/alternative_facts.js" charset="utf-8"></script>
-  <script src="js/jquery.min.js" charset="utf-8"></script>
-  <script src="js/bootstrap.min.js" charset="utf-8"></script>
-</head>
-
-<body>
-  <nav class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button 
-          type="button"
-          class="navbar-toggle collapsed"
-          data-toggle="collapse"
-          data-target="#navbar-collapse"
-          aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a href="/" class="navbar-brand">Chris <span id="bespoke-fact"></span></a>
-      </div>
-
-      <div class="collapse navbar-collapse" id="navbar-collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="./">Home</a></li>
-          <li><a href="./about">About</a></li>
-          <li><a href="./posts">Posts</a></li>
-          <li><a href="./cv">Resume</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+<?php
+  define('__ROOT__', dirname(__FILE__));
+  require_once(__ROOT__.'/header.php');
+?>
 
   <div class="container-fluid">
     <div class="col-md-8"></div>
