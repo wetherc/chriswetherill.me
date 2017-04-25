5d57aa5cf8aaf8fe0295feb6d7f694fa1c568985
faulconbridge <chris@tbmh.org>
2017-04-25 13:27:56 -0400

    HTML > PHP

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  define('__ROOT__', dirname(dirname(__FILE__)));
+  require_once(__ROOT__.'/header.php');
+?>
+
+  <div class="container-fluid">
+    <div class="col-md-8"></div>
+  </div>
+</body>
+</html>
