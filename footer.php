d49eafb73d8ed9acc6a927bcef5178aeddbc43e0
faulconbridge <chris@tbmh.org>
2017-04-25 15:16:35 -0400

    Added footer/sidebar

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <div class="col-md-3" style="border-left: solid 1px #2675D1;">
+      <h2 style="color: #2F353B;">Chris Wetherill</h2>
+      <p>Data scientist at <a href="https://www.safeauto.com" target="_blank">SafeAuto</a>. Previously at Virginia Tech and the Cleveland Clinic.</p>
+      <p>Follow me on:</p>
+      <span>
+        <a href="https://github.com/faulconbridge" target="_blank"><img src="./images/github.svg" width="57px" height="57px" /></a>
+        <a href="https://www.linkedin.com/in/christopherwetherill" target="_blank"><img src="./images/linkedin.svg" width="57px" height="57px" /></a>
+        <a href="https://facebook.com/christopher.wetherill/about" target="_blank"><img src="./images/facebook.svg" width="57px" height"57px" /></a>
+        <a href="https://plus.google.com/+ChristopherWetherill/about" target="_blank"><img src="./images/gplus.svg" width="57px" height="57px" /></a>
+      </span>
+    </div>
+  </div>
+</body>
+</html>
