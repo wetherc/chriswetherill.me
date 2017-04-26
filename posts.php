a7b688c4cfb993973619b566949ed4f073de8c60
faulconbridge <chris@tbmh.org>
2017-04-26 19:32:33 -0400

    CSS tweaks

--- posts.php
+++ posts.php
@@ -3,7 +3,7 @@
   require_once(__ROOT__.'/header.php');
 ?>
 
-<div class="col-md-8">
+<div class="col-md-6 col-md-offset-1">
   <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam turpis et efficitur elementum. Ut dictum eu diam non elementum. Morbi laoreet libero sed pharetra luctus. Etiam ante magna, auctor nec enim nec, laoreet consequat lorem. Ut aliquam libero nec enim iaculis, eget feugiat sapien consequat. Mauris est enim, fermentum eu ornare id, porttitor faucibus augue.</p>
 
   <p>Fusce sed mattis eros. Vivamus at varius justo. Nunc fringilla ac diam congue faucibus. Proin ullamcorper imperdiet nisi, vel mollis est fringilla sit amet. Mauris tempus in quam non venenatis. Curabitur sit amet eleifend lectus. Vestibulum consectetur nec est at cursus. Fusce dignissim nibh et sapien auctor hendrerit quis at urna. Nullam nibh erat, interdum eu arcu et, placerat luctus nunc. Mauris tincidunt eu purus tempus egestas. Maecenas commodo, leo eu euismod tempus, orci risus hendrerit arcu, ut molestie sem purus a mauris. Donec luctus massa sapien, eget porta sem dignissim ac. Vestibulum nec efficitur nulla. Aenean pulvinar faucibus placerat.</p>
