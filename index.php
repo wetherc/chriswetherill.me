7f975e57a4bee956f3902b2357a7ad94eabc7b85
faulconbridge <chris@tbmh.org>
2017-04-26 16:29:12 -0400

    Some generic about me crap

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
   require_once(__ROOT__.'/header.php');
 ?>
 
-<div class="col-md-8">
-  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam turpis et efficitur elementum. Ut dictum eu diam non elementum. Morbi laoreet libero sed pharetra luctus. Etiam ante magna, auctor nec enim nec, laoreet consequat lorem. Ut aliquam libero nec enim iaculis, eget feugiat sapien consequat. Mauris est enim, fermentum eu ornare id, porttitor faucibus augue.</p>
+<div class="col-md-6 col-md-offset-1">
+  <h1>Hey! You found me.</h2>
+  <br />
 
-  <p>Fusce sed mattis eros. Vivamus at varius justo. Nunc fringilla ac diam congue faucibus. Proin ullamcorper imperdiet nisi, vel mollis est fringilla sit amet. Mauris tempus in quam non venenatis. Curabitur sit amet eleifend lectus. Vestibulum consectetur nec est at cursus. Fusce dignissim nibh et sapien auctor hendrerit quis at urna. Nullam nibh erat, interdum eu arcu et, placerat luctus nunc. Mauris tincidunt eu purus tempus egestas. Maecenas commodo, leo eu euismod tempus, orci risus hendrerit arcu, ut molestie sem purus a mauris. Donec luctus massa sapien, eget porta sem dignissim ac. Vestibulum nec efficitur nulla. Aenean pulvinar faucibus placerat.</p>
+  <p>I'm Chris Wetherill, a Midwestern kid working on fun data-related things at SafeAuto. I founded and currently lead our Data & Decision Science team: we do stuff somewhere between data engineering, machine learning, and software engineering, but are generally focused on modernizing how SafeAuto consumes, interprets, and acts on data. Mostly this is just an excuse to do cool things like build self-service analytics tools or design and stand up a Hadoop cluster. Saying it out loud, though, maybe I need better hobbies...</p>
 
-  <p> Proin faucibus nisi vel nunc viverra pulvinar. Curabitur sed imperdiet ex. Suspendisse potenti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac mollis tortor. Maecenas fringilla nibh dolor. Cras placerat nisl interdum elit porta finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+  <p>Prior to this, I was working my way through a PhD program in computational biology at Virginia Tech. We did some cool work at the intersection of genomics and epidemiolgy, trying to better predict how diseases move through populations.</p>
+
+  <p>Every now and then, I'll even feel super motivated and make some contributions on <a href="https://github.com/faulconbridge">GitHub</a>. Right now I'm working on Cast, a cluster management utility that's arguably slightly better than running SSH in a <code>for</code> loop.</p>
+
+  <p>Wanna chat about data? Feel free to <a href="mailto:chris@tbmh.org">get in touch!</a></p>
+
+  <h2>Projects</h2>
+  <br />
+
+  <p>Some of the stuff I've worked on:</p>
+  <ul>
+    <li><a href="https://github.com/faulconbridge/cast">Cast</a>: A cluster management utility for wrecking more servers even faster.</li>
+    <li><a href="https://github.com/Vervidium/vervidium">Vervidium</a>: A platform for data science and self-service analytics.</li>
+    <li><a href="https://github.com/infoactive/data-design">Data + Design</a>: A book aimed at journalists on how to responsibly present and visualize data. I edited this before alternative facts really came into fashion.</li>
+  </ul>
+
 </div>
 
 <?php
