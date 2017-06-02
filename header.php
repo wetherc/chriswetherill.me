1e67b49237c33ccb929095c48679ce1eb238a0b1
faulconbridge <chris@chriswetherill.me>
2017-06-01 20:51:20 -0400

    Restore favicon link

--- header.php
+++ header.php
@@ -10,6 +10,8 @@
 
   <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
   <link rel="stylesheet" href="css/custom.css" type="text/css" />
+  <link rel="shortcut icon" href="images/favicon.png" />
+
   <script src="js/alternative_facts.js" charset="utf-8"></script>
   <script src="js/jquery.min.js" charset="utf-8"></script>
   <script src="js/bootstrap.min.js" charset="utf-8"></script>
