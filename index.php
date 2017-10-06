eb338475082a13c8778e4018ecb55b33bc06597d
faulconbridge <chris@chriswetherill.me>
2017-10-06 19:28:28 -0400

    Updated index

--- index.php
+++ index.php
@@ -7,22 +7,26 @@
   <h1>Hey! You found me.</h2>
   <br />
 
-  <p>I'm Chris Wetherill, a Midwestern kid working on fun data-related things at SafeAuto. I founded and currently lead our Data & Decision Science team: we do stuff somewhere between data engineering, machine learning, and software engineering, but are generally focused on modernizing how SafeAuto consumes, interprets, and acts on data. Mostly this is just an excuse to do cool things like build self-service analytics tools or design and stand up a Hadoop cluster. Saying it out loud, though, maybe I need better hobbies...</p>
+  <p>I&rsquo;m Chris Wetherill, a Midwestern kid working on fun data-related things at SafeAuto. I started off as the inaugural member of our Data &amp; Decision Science team and am currently the lead data scientist on our R&amp;D team. We do stuff somewhere in the neighborhood of data engineering, machine learning, and software engineering, but are generally focused on modernizing how SafeAuto consumes, interprets, and acts on data.</p>
+  
+  <p>Or, you know, we sell auto insurance.</p>
+  
+  <p>Mostly this is just an excuse for us to do cool things like build self-service analytics tools, dig into machine learning algorithms, find new and exciting ways to quanatify risk, and maybe develop an API here or there. It&rsquo;s a hoot.</p>
 
-  <p>Prior to this, I was working my way through a PhD program in computational biology at Virginia Tech. We did some cool work at the intersection of genomics and epidemiolgy, trying to better predict how diseases move through populations.</p>
+  <p>Prior to this, I was working my way through a PhD program in computational biology at Virginia Tech. We did some work at the intersection of genomics and epidemiolgy, trying to better predict how diseases move through populations.</p>
 
-  <p>Every now and then, I'll even feel super motivated and make some contributions on <a href="https://github.com/faulconbridge">GitHub</a>. Right now I'm working on Cast, a cluster management utility that's arguably slightly better than running SSH in a <code>for</code> loop.</p>
+  <p>Every now and then, I&rsquo;ll even feel super motivated and make some contributions on <a href="https://github.com/faulconbridge">GitHub</a>. Right now I&rsquo;m working on Cast, a cluster management utility that&rsquo;s arguably slightly better than running SSH in a <code>for</code> loop.</p>
 
-  <p>Wanna chat about data? Feel free to <a href="mailto:chris@tbmh.org">get in touch!</a></p>
+  <p>Wanna chat about data? Feel free to <a href="mailto:hello@c18l.org">get in touch!</a></p>
 
   <h2>Projects</h2>
   <br />
 
-  <p>Some of the stuff I've worked on:</p>
+  <p>Some of the stuff I&rsquo;ve worked on:</p>
   <ul>
     <li><a href="https://github.com/faulconbridge/cast">Cast</a>: A cluster management utility for wrecking more servers even faster.</li>
-    <li><a href="https://github.com/Vervidium/vervidium">Vervidium</a>: A platform for data science and self-service analytics.</li>
-    <li><a href="https://github.com/infoactive/data-design">Data + Design</a>: A book aimed at journalists on how to responsibly present and visualize data. I edited this before alternative facts really came into fashion.</li>
+    <li><a href="https://github.com/faulconbridge/invino">Invino</a>: A wine recommendation engine based on science, not sommeliers.</li>
+    <li><a href="https://github.com/infoactive/data-design">Data + Design</a>: A book aimed at journalists on how to responsibly present and visualize data. I edited this before alternative facts really came into their own.</li>
   </ul>
 
 </div>
