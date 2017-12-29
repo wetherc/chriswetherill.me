6ad3cd12a4b61349aebcb0a1abc3ec6caab1d584
faulconbridge <chris@chriswetherill.me>
2017-12-29 12:11:52 -0500

    Updated about me

--- index.php
+++ index.php
@@ -7,17 +7,15 @@
   <h1>Hey! You found me.</h2>
   <br />
 
-  <p>I&rsquo;m Chris Wetherill, a Midwestern kid working on fun data-related things at SafeAuto. I started off as the inaugural member of our Data &amp; Decision Science team and am currently the lead data scientist on our R&amp;D team. We do stuff somewhere in the neighborhood of data engineering, machine learning, and software engineering, but are generally focused on modernizing how SafeAuto consumes, interprets, and acts on data.</p>
+  <p>I&rsquo;m Chris Wetherill, a Midwestern kid working on fun data-related things at State Auto Insurance Companies. Here, I lead data science and am responsible for developing partnerships and strategic collaborations with other analytic groups across the company.</p>
   
-  <p>Or, you know, we sell auto insurance.</p>
-  
-  <p>Mostly this is just an excuse for us to do cool things like build self-service analytics tools, dig into machine learning algorithms, find new and exciting ways to quanatify risk, and maybe develop an API here or there. It&rsquo;s a hoot.</p>
-
+  <p>Previously, I was at SafeAuto Insurance where I was the founding and lead member of their Data &amp; Decision Science team. There, I led pilot programs to evaluate new and emerging technologies and partnered with business stakeholders to develop and implement new predictive models. Following this, I transitioned to a technical lead role on our Product R&amp;D team where I investigated ways to improve customer segmentation in our rating algorithm and led the development of new underwriting models.</p>
+    
   <p>Prior to this, I was working my way through a PhD program in computational biology at Virginia Tech. We did some work at the intersection of genomics and epidemiolgy, trying to better predict how diseases move through populations.</p>
 
   <p>Every now and then, I&rsquo;ll even feel super motivated and make some contributions on <a href="https://github.com/faulconbridge">GitHub</a>. Right now I&rsquo;m working on Cast, a cluster management utility that&rsquo;s arguably slightly better than running SSH in a <code>for</code> loop.</p>
 
-  <p>Wanna chat about data? Feel free to <a href="mailto:hello@c18l.org">get in touch!</a></p>
+  <p>Want to chat about data? Feel free to <a href="mailto:hello@c18l.org">get in touch!</a></p>
 
   <h2>Projects</h2>
   <br />
