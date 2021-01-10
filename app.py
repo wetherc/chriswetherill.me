d9883b87e7d22a0304f78b7ea7b72907842a15f0
Christopher Wetherill <chris@tbmh.org>
2021-01-10 14:43:24 -0500

    Migrate some JS snippets into Python

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 import yaml
+import secrets
 import os
 from pathlib import Path
 from flask import Flask, request, render_template
+from typing import List
 
 
 APP_ROOT = Path(os.path.realpath(os.path.expanduser(__file__))).parents[0]
 app = Flask(__name__)
 
+with open(APP_ROOT / 'content/alternative_facts.yml', 'r') as facts:
+    FACTS = yaml.safe_load(facts)
+
+
+def pull_alternative_fact(fact_list: List[str]) -> str:
+    return f'Chris {secrets.choice(fact_list)}'
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    _fact = pull_alternative_fact(FACTS)
+    return render_template('index.html', fact=_fact)
 
 
 @app.route('/resume', methods=['GET'])
 def resume():
     with open(APP_ROOT / 'content/cv.yml', 'r') as data:
         parsed_cv = yaml.safe_load(data)
-    return render_template('resume.html', data=parsed_cv)
+
+    _fact = pull_alternative_fact(FACTS)
+    return render_template('resume.html', fact=_fact, data=parsed_cv)
 
 
 if __name__ == '__main__':
